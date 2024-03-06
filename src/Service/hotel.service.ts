1e511ac1ee51e0be8ce5ba68353df9b5fa6fe48c
Valentin <75315884+Contunder@users.noreply.github.com>
2024-03-06 17:04:47 +0100

    Update hotel.service.ts

--- src/Service/hotel.service.ts
+++ src/Service/hotel.service.ts
@@ -6,7 +6,11 @@ const API_URL = "http://localhost:8080/api/hotel";
 
 export const getAllHotel = async (): Promise<PostType> => {
     try {
-        const response = await axios.get(API_URL + "/get", headersConfig);
+        const response = await axios.get(API_URL + "/get", {headers: {
+            'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }});
         return response.data;
     } catch (error) {
         console.error(error);
